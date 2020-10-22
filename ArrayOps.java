a926385f09058d1ee6489c728ba7446ef5e71d7c
Andy Lin <alin21@stuy.edu>
2020-10-22 13:50:00 -0400

    Big fixing

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,12 +1,12 @@
 class ArrayOps{
-  public int[] sum(int[] arr) {
+  public static int sum(int[] arr) {
     int total = 0;
     for (int i = 0; i<arr.length; i++){
       total += arr[i];
     }
     return total;
   }
-  public int[] largest(int[]arr) {
+  public static int largest(int[]arr) {
     int highest = 0;
     for (int i = 0; i<arr.length; i++){
       highest = Math.max(highest,arr[i]);
@@ -15,15 +15,15 @@ class ArrayOps{
   }
   public static int[] sumRows(int[][] matrix) {
     int[] result = new int[matrix.length];
-    for (int i = 0; i<arr.length; i++){
-      result[i] = sum(result[i]);
+    for (int i = 0; i<matrix.length; i++){
+      result[i] = sum(matrix[i]);
     }
     return result;
   }
   public static int[] largestInRows(int[][] matrix) {
     int[] result = new int[matrix.length];
-    for (int i = 0; i<arr.length; i++){
-      result[i] = largest(result[i]);
+    for (int i = 0; i<matrix.length; i++){
+      result[i] = largest(matrix[i]);
     }
     return result;
   }
--- Tester.java
+++ Tester.java
@@ -1,10 +1,13 @@
 class Tester{
+  static int[] test1 = new int [] {1,2,3,4,5,6};
+  static int[] test2 = new int [] {191,181,175,168,145,125};
+  static int[][] test3 = new int [][] {{1,2,3,4,5,6}, {191,181,175,168,145,125}};
   public static void main(String[] args) {
-    static int[] test1 = [1,2,3,4,5,6];
-    static int[] test2 = [191,181,175,168,145,125];
     System.out.println(ArrayOps.sum(test1));
     System.out.println(ArrayOps.sum(test2));
-    System.out.println(ArrayOps.highest(test1));
-    System.out.println(ArrayOps.highest(test2));
+    System.out.println(ArrayOps.largest(test1));
+    System.out.println(ArrayOps.largest(test2));
+    System.out.println(ArrayOps.sumRows(test3));
+    System.out.println(ArrayOps.largestInRows(test3));
   }
 }
