20f7eb3b5a0580cd2c9b5a9aa5a8eaca4f108015
Andy Lin <alin21@stuy.edu>
2020-10-22 13:09:23 -0400

    largest func added

--- ArrayOps.java
+++ ArrayOps.java
@@ -6,4 +6,11 @@ class ArrayOps{
     }
     return total;
   }
+  public int[] largest(int[]arr) {
+    int highest = 0;
+    for (int i = 0; i<arr.length; i++){
+      highest = Math.max(highest,arr[i]);
+    }
+    return highest;
+  }
 }
