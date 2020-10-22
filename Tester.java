c64d7f84e85d1aa324b0ac904ec875ffd1c31a36
Andy Lin <alin21@stuy.edu>
2020-10-22 13:18:09 -0400

    Inital version of tester

---
+++ Tester.java
@@ -0,0 +1,10 @@
+class Tester{
+  public static void main(String[] args) {
+    static int[] test1 = [1,2,3,4,5,6];
+    static int[] test2 = [191,181,175,168,145,125];
+    System.out.println(ArrayOps.sum(test1));
+    System.out.println(ArrayOps.sum(test2));
+    System.out.println(ArrayOps.highest(test1));
+    System.out.println(ArrayOps.highest(test2));
+  }
+}
